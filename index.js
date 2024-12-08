bf3a6b1ea796b99e6410757702aa893f627036b8
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-08 22:02:58 +0600

    1st function done

---
+++ index.js
@@ -0,0 +1,11 @@
+// Frist function Calculate Difference
+
+function calculateDifference(a, b) {
+    return a - b;
+}
+console.log(
+    calculateDifference(9, 5)
+);
+console.log(
+    calculateDifference(56, 8)
+);
